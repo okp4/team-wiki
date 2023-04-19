40ce49fa523ded9736630693ba18821274f842d1
JoeyDjr <JoeyDjr@users.noreply.github.com>
2023-04-19 14:22:52 +0000

    deploy: 5562815cd6aee4e4d3b6540ce4551744a3f1142d

--- fr/assets/js/b39a5bfd.8fc3563d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_okp4_team_wiki=self.webpackChunk_okp4_team_wiki||[]).push([[738],{367:e=>{e.exports=JSON.parse('{"title":"Vision","slug":"/category/vision","permalink":"/fr/category/vision","navigation":{"next":{"title":"Pourquoi avons-nous besoin de partager des donn\xe9es ?","permalink":"/fr/vision/data"}}}')}}]);
